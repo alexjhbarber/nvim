d2a9e2b75183dd1f49ef06ac1cb0afbac9a0ccc0
AJHBarber <alex8mpn@gmail.com>
2025-06-16 13:22:12 +0100

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return { 
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    prefer_git = false,
+    compilers = {'zig'},
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "lua", "javascript", "php", "python", "markdown" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end, 
+}
+
